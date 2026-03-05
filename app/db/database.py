3bf056dac03074bab5ad65e8d167f3f883b53ac0
Matt <matthew.n_2023_lon@outlook.com>
2026-03-05 13:34:52 +0000

    feature: added github actions yaml file including test trivy image scan and deploy

--- app/db/database.py
+++ app/db/database.py
@@ -4,22 +4,21 @@ from dotenv import load_dotenv
 import os
 import sqlite3
 
-load_dotenv()
-
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
 
 # Enable foreign key constraints for SQLite
 @event.listens_for(engine, "connect")
 def enable_foreign_keys(dbapi_connection, connection_record):
-    if isinstance(dbapi_connection, sqlite3.Connection): 
+    if isinstance(dbapi_connection, sqlite3.Connection):
         cursor = dbapi_connection.cursor()
         cursor.execute("PRAGMA foreign_keys=ON")
         cursor.close()
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
